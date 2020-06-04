e1dd0f8641a1bc5fdb8874264e05012c8f9ecf48
Perham <perham132@hotmail.se>
2020-06-04 15:22:47 +0200

    test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fileupload = require('express-fileupload');
 const app = express();
 
 let  port = process.env.PORT;
-ffmpeg.setFfmpegPath("/bin/ffmpeg.exe");
+ffmpeg.setFfmpegPath("D:\\ADX converter\\bin\\ffmpeg.exe");
 
 app.use(fileupload({
     useTempFiles:true,
